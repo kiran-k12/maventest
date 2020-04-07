8b869e88af735cf5a5449230505a0f3377449dc3
kiran-k12 <62979107+kiran-k12@users.noreply.github.com>
2020-04-07 15:30:09 +0530

    Updated App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "New Code Smell" );// This is the new code added by Kiran
     }
 }
