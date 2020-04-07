a9ebc6ecf0a40b5213c8a9ddd2b401830474d58f
kiran-k12 <62979107+kiran-k12@users.noreply.github.com>
2020-04-07 17:40:03 +0530

    updated App.java by adding duplicate lines

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
         System.out.println( "New Code Smell" );// This is the new code added by Kiran
     }
 }
