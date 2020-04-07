7e23a47d88645c81d3bda8e8f940d672ef2e3660
kiran-k12 <62979107+kiran-k12@users.noreply.github.com>
2020-04-07 17:55:44 +0530

    removed ; in App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,7 +11,7 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" )
         System.out.println( "New Code Smell" );// This is the new code added by Kiran
     }
 }
